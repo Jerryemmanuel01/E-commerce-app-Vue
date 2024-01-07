455c87acf42d70e87a151e57ef42eb3230b2bb21
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-01-07 18:44:41 +0100

    pages completely added

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
 <template>
-  <h1>hello from vue</h1>
+  <h1>Falcons Olive Oil</h1>
+  <div class="page-wrap">
+    <router-view />
+  </div>
+  
 </template>
 
-<script>
-export default {
-  name: "App",
-  components: {},
-};
+<script setup>
+// export default {
+//   name: "App",
+//   components: {},
+// };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/pages/ProductPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Product</h1>
+  <div class="grid-wrap">
+    <div class="product-item" v-for="product in products" :key="product.id">
+      <img :src="product.imageName" alt="" />
+      <h3 class="product-name">{{ product.name }}</h3>
+      <p class="product-price">{{ product.price }}</p>
+      <button>View Details</button>
+    </div>
+  </div>
+</template>
+<script setup>
+import { products } from "../temp-data";
+// export default {
+//   name: "ProductPage",
+//   data() {
+//     return {
+//       products,
+//     };
+//   },
+// };
+</script>
